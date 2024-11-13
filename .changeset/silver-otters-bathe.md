7a02de0c9f7eb0544a76624ebfb92cdff9c8787e
Ben Fein <ben_fein@icloud.com>
2024-11-13 16:55:41 -0700

    fixed textarea component by using relative input

---
+++ .changeset/silver-otters-bathe.md
@@ -0,0 +1,5 @@
+---
+"@lambdacurry/forms": minor
+---
+
+Fixed Textarea by using relative import
--- packages/components/src/ui/textarea.tsx
+++ packages/components/src/ui/textarea.tsx
@@ -1,5 +1,5 @@
 import { type TextareaHTMLAttributes, forwardRef } from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 export interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
