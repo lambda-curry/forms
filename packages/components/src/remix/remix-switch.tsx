7c6004748139156708dd05c23312fad8269de2da
Jake Ruesink <jaruesink@gmail.com>
2024-11-25 22:08:54 -0600

    fix: applying code rabbit suggestion on memoizing custom components that can get passed into remix form elements

--- packages/components/src/remix/remix-switch.tsx
+++ packages/components/src/remix/remix-switch.tsx
@@ -1,8 +1,7 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { useMemo, type ComponentPropsWithoutRef } from 'react';
 import { useRemixFormContext } from 'remix-hook-form'
 import { Switch } from '../ui/switch'
 import { RemixFormControl, RemixFormDescription, RemixFormLabel, RemixFormMessage } from './remix-form';
-import type { FieldComponents } from '../ui/form';
 
 export interface RemixSwitchProps extends Omit<ComponentPropsWithoutRef<typeof Switch>, 'control'> {
   name: string;
@@ -13,12 +12,15 @@ export interface RemixSwitchProps extends Omit<ComponentPropsWithoutRef<typeof S
 export function RemixSwitch({ name, label, description, ...props }: RemixSwitchProps) {
   const { control } = useRemixFormContext();
 
-  const components: Partial<FieldComponents> = {
-    FormDescription: RemixFormDescription,
-    FormControl: RemixFormControl,
-    FormLabel: RemixFormLabel,
-    FormMessage: RemixFormMessage,
-  };
+  const components = useMemo(
+    () => ({
+      FormDescription: RemixFormDescription,
+      FormControl: RemixFormControl,
+      FormLabel: RemixFormLabel,
+      FormMessage: RemixFormMessage,
+    }),
+    []
+  );
 
   return (
     <Switch
--- packages/components/src/remix/remix-text-field.tsx
+++ packages/components/src/remix/remix-text-field.tsx
@@ -1,6 +1,6 @@
+import { useMemo } from 'react';
 import { useRemixFormContext } from 'remix-hook-form';
 import { TextField, type TextFieldProps } from '../ui/text-field';
-import type { FieldComponents } from '../ui/form';
 import { RemixFormControl, RemixFormDescription, RemixFormLabel, RemixFormMessage } from './remix-form';
 
 export type RemixTextFieldProps = Omit<TextFieldProps, 'control'>;
@@ -8,12 +8,15 @@ export type RemixTextFieldProps = Omit<TextFieldProps, 'control'>;
 export function RemixTextField(props: RemixTextFieldProps) {
   const { control } = useRemixFormContext();
 
-  const components: Partial<FieldComponents> = {
-    FormControl: RemixFormControl,
-    FormLabel: RemixFormLabel,
-    FormDescription: RemixFormDescription,
-    FormMessage: RemixFormMessage,
-  };
+  const components = useMemo(
+    () => ({
+      FormControl: RemixFormControl,
+      FormLabel: RemixFormLabel,
+      FormDescription: RemixFormDescription,
+      FormMessage: RemixFormMessage,
+    }),
+    []
+  );
 
   return <TextField control={control} components={components} {...props} />;
 }
