7969d2fb1e5b49499263f51696aadb0884e9740e
Jake Ruesink <jaruesink@gmail.com>
2024-12-20 02:05:54 -0600

    chore: update package.json and tsconfig.json for improved module structure
    - Refactored package.json to change the "source" and "main" entries to "type" and "exports", aligning with ES module standards.
    - Updated paths in tsconfig.json to reflect the new directory structure, changing output to "dist" and adjusting module resolution settings.
    - Added new dependencies for better compatibility and updated existing ones for consistency.

---
+++ packages/components/vite.config.ts
@@ -0,0 +1,80 @@
+import { extname, relative } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import react from '@vitejs/plugin-react';
+import { glob } from 'glob';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: {
+        index: './src/index.ts',
+        remix: './src/remix/index.ts',
+        ui: './src/ui/index.ts',
+      },
+      formats: ['es'],
+    },
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync('src/**/*.{ts,tsx}', {
+            ignore: ['src/**/*.d.ts'],
+          })
+          .map((file) => [
+            // The name of the entry point
+            // src/nested/foo.ts becomes nested/foo
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // src/nested/foo.ts becomes /project/src/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      external: [
+        'react',
+        'react/jsx-runtime',
+        '@radix-ui/react-alert-dialog',
+        '@radix-ui/react-avatar',
+        '@radix-ui/react-checkbox',
+        '@radix-ui/react-dialog',
+        '@radix-ui/react-dropdown-menu',
+        '@radix-ui/react-icons',
+        '@radix-ui/react-label',
+        '@radix-ui/react-popover',
+        '@radix-ui/react-radio-group',
+        '@radix-ui/react-scroll-area',
+        '@radix-ui/react-slot',
+        '@radix-ui/react-switch',
+        '@radix-ui/react-tooltip',
+        '@remix-run/node',
+        '@remix-run/react',
+        '@remix-run/serve',
+        'class-variance-authority',
+        'clsx',
+        'date-fns',
+        'input-otp',
+        'lucide-react',
+        'next-themes',
+        'react-day-picker',
+        'react-hook-form',
+        'remix-hook-form',
+        'sonner',
+        'tailwind-merge',
+        'tailwindcss-animate',
+        'zod',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+});
