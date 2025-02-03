b668f74ec5b8fd37085db7da69eb0fb237be706e
Ben Fein <ben_fein@icloud.com>
2025-02-03 18:37:17 -0500

    feat: added the ability to pass in your own indicator to the radio group item

---
+++ .changeset/five-bobcats-remain.md
@@ -0,0 +1,5 @@
+---
+"@lambdacurry/forms": patch
+---
+
+Added the ability to pass in your own indicator to the radio group
--- packages/components/src/ui/radio-group.tsx
+++ packages/components/src/ui/radio-group.tsx
@@ -15,8 +15,10 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> & {
+    indicator?: React.ReactNode;
+  }
+>(({ className, indicator, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
@@ -27,7 +29,7 @@ const RadioGroupItem = React.forwardRef<
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-2.5 w-2.5 fill-current text-current" />
+        {indicator || <Circle className="h-2.5 w-2.5 fill-current text-current" />}
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
